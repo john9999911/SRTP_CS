1b32f08b23cca8776995156695d005a38f09fb83
30838 <3083842408@qq.com>
2022-03-25 22:00:16 +0800

    3.25 结果Controller

--- dev/src/main/java/com/srtp_cs/dev/controller/Search/SearchController.java
+++ dev/src/main/java/com/srtp_cs/dev/controller/Search/SearchController.java
@@ -2,19 +2,21 @@ package com.srtp_cs.dev.controller.Search;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class SearchController {
 
     @RequestMapping("search_result")
     public String SearchResult(@RequestParam("resultNum") int resultNum, Model model){
-
-
-
-
+        String[] result = new String[resultNum];
+        for (int i = 0; i < resultNum; i++) {
+            result[i] = "result_" + i;
+        }
+        model.addAttribute("result", result);
         return "search_result";
+
     }
 }
