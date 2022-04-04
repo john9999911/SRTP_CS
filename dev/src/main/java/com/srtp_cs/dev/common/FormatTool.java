363565e5c9ac3152aa96e2b90c2c4d3043161536
30838 <3083842408@qq.com>
2022-04-04 23:44:12 +0800

    4.4 代码格式化（未完成）

---
+++ dev/src/main/java/com/srtp_cs/dev/common/FormatTool.java
@@ -0,0 +1,21 @@
+//package com.srtp_cs.dev.common;
+//
+//
+//import com.google.googlejavaformat.java.Formatter;
+//import com.google.googlejavaformat.java.FormatterException;
+//
+//public class FormatTool {
+//
+//
+//    public static void main(String[] args) throws FormatterException {
+//        Formatter formatter = new Formatter();
+//        String source = "public class FormatTool {\n" +
+//                "\n" +
+//                "    public static void main(String[] args) {\n" +
+//                "        System.out.println(\"Hello World!\");\n" +
+//                "    }\n" +
+//                "}";
+//        String formattedSource = formatter.formatSource(source);
+//        System.out.println(formattedSource);
+//    }
+//}
