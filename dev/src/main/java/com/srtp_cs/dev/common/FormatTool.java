0cf86a44284155c7f72c9b0e841f358042d47e66
30838 <3083842408@qq.com>
2022-04-07 12:06:49 +0800

    4.7 代码格式化（已完成）

--- dev/src/main/java/com/srtp_cs/dev/common/FormatTool.java
+++ dev/src/main/java/com/srtp_cs/dev/common/FormatTool.java
@@ -1,21 +1,36 @@
-//package com.srtp_cs.dev.common;
-//
-//
-//import com.google.googlejavaformat.java.Formatter;
-//import com.google.googlejavaformat.java.FormatterException;
-//
-//public class FormatTool {
-//
-//
-//    public static void main(String[] args) throws FormatterException {
-//        Formatter formatter = new Formatter();
-//        String source = "public class FormatTool {\n" +
-//                "\n" +
-//                "    public static void main(String[] args) {\n" +
-//                "        System.out.println(\"Hello World!\");\n" +
-//                "    }\n" +
-//                "}";
-//        String formattedSource = formatter.formatSource(source);
-//        System.out.println(formattedSource);
-//    }
-//}
+package com.srtp_cs.dev.common;
+
+
+import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
+import com.google.googlejavaformat.java.JavaFormatterOptions;
+
+public class FormatTool {
+    JavaFormatterOptions.Style style;
+    JavaFormatterOptions options;
+    Formatter formatter;
+
+    public FormatTool() {
+        style = JavaFormatterOptions.Style.AOSP;
+        options = JavaFormatterOptions.builder().style(style).build();
+        formatter = new Formatter(options);
+    }
+
+    public String format(String source) {
+        try {
+            return formatter.formatSource(source);
+        } catch (FormatterException e) {
+            e.printStackTrace();
+            return source;
+        }
+    }
+    public static void main(String[] args) {
+        FormatTool formatTool = new FormatTool();
+        String source = "public class FormatTool { public static void main(String[] args) { System.out.println(\"Hello World!\");}}";
+        String formattedSource = formatTool.format(source);
+        System.out.println(formattedSource);
+    }
+}
+
+
+
